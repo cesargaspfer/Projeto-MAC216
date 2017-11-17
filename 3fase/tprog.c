4c0f900f414102928fac2b9386927116573f5e01
dsnunes <dannunes3451@gmail.com>
2017-11-17 18:58:17 -0200

    Falta o fim()

--- 3fase/tprog.c
+++ 3fase/tprog.c
@@ -2,12 +2,5 @@
 #include "maq.h"
 INSTR prog[] = {
 {CLT, ACAO, {EAST}},
-{CLT, ACAO, {NORTHEAST}},
-{CLT, ACAO, {WEST}},
-{CLT, ACAO, {SOUTHWEST}},
-{PRN, NONE, {0}},
-{PRN, NONE, {0}},
-{PRN, NONE, {0}},
-{PRN, NONE, {0}},
 {END, NONE, {0}},
 };
