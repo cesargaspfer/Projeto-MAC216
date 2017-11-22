061a959e59c13ed0f4719b2440bb207c14194456
dsnunes <dannunes3451@gmail.com>
2017-11-21 22:00:23 -0200

    Criação da pasta 4fase e let's go

---
+++ 4fase/maq.h
@@ -0,0 +1,39 @@
+#ifndef MAQ_H
+#define MAQ_H
+#include "pilha.h"
+#define MAXMEM 100
+
+typedef struct {
+  Pilha pil;
+  Pilha exec;
+  OPERANDO Mem[MAXMEM];
+  INSTR *prog;
+  int ip;
+  // atributos concretos de um robo //
+  // saúde (diminui com avarias)
+  int vida;
+  // coordenada da célula em que está
+  int posx;
+  int posy;
+  // número de cristais que carrega
+  int crist;
+  int energia;
+  // time ao qual pertence
+  int exercito;
+  // dano que o robô causa quando ataca outro robô (decréscimo na "vida" de outro robô)
+  // padrão: 10
+  int dano;
+} Maquina;
+
+// declaração da função cria_maquina
+Maquina *cria_maquina(INSTR *p, int posX, int posY, int TimeV);
+void destroi_maquina(Maquina *m);
+// declaração da função exec_maquina
+void exec_maquina(Maquina *m, int n);
+
+INSTR *geraProg();
+
+
+void Sistema(int op, int dir,  Maquina *m);
+void Atualiza ();
+#endif /* MAQ_H*/
